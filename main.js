feacb5a0629979128337de4de6fb50f4bdf3aaae
Andreas <asc4asc@users.noreply.github.com>
2020-12-30 17:43:09 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import * as c1 from './lib/counter.js';
+import inc from './lib/counter.js';
+  
+console.log("Info1:",c1.counter); // 1
+c1.increment();
+console.log("Info2:",c1.counter); // 2
+c1.decrement();  
+c1.decrement();  
+console.log("Info3:",c1.counter); // 0  
+inc();
+console.log("Info4:",c1.counter); // 1
